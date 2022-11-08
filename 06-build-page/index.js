d2bf92a0fb5c5312cc0f95d3e882104bd1136d14
Sosedova Viktoria <nvikamail@mail.ru>
2022-11-08 13:21:21 +0400

    feat: add functional for 6 task full

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,7 +5,6 @@ const path_file_copy = path.join(__dirname, 'project-dist');
 const folder_assets = path.join(__dirname, 'project-dist/assets');
 const path_files = path.join(__dirname, 'assets');
 
-
 fs.mkdir(path_file_copy, { recursive: true }, err => {
   if (err) {
     throw err;
@@ -14,12 +13,12 @@ fs.mkdir(path_file_copy, { recursive: true }, err => {
     if (err) {
       throw err;
     }
-    fs.readdir(path_files, {withFileTypes: true}, (err, files) => {
+    fs.readdir(path_files, { withFileTypes: true }, (err, files) => {
       files.forEach(file => {
         const name_file = path.basename(file.name);
-        if(file.isDirectory()){
-          fs.mkdir(folder_assets+'\\'+name_file, { recursive: true }, () => {});
-          fs.readdir(path_files+'\\'+name_file, {withFileTypes: true}, (err, fls) => {
+        if (file.isDirectory()) {
+          fs.mkdir(folder_assets + '\\' + name_file, { recursive: true }, () => { });
+          fs.readdir(path_files + '\\' + name_file, { withFileTypes: true }, (err, fls) => {
             fls.forEach(fl => {
               fs.copyFile(`${path_files}\\${name_file}\\${fl.name}`, `${folder_assets}\\${name_file}\\${fl.name}`, () => { });
             });
@@ -29,42 +28,48 @@ fs.mkdir(path_file_copy, { recursive: true }, err => {
     });
   });
 });
+
 const path_folder = path.join(__dirname, 'styles');
 fs.createWriteStream(path.join(__dirname, 'project-dist/style.css')).write('');
-fs.readdir(path_folder, {withFileTypes: true}, (err, files) => {
+fs.readdir(path_folder, { withFileTypes: true }, (err, files) => {
   files.forEach(file => {
     const name_file_with_ext = path.basename(file.name);
     const exten = path.extname(name_file_with_ext);
-    if(exten === '.css'){
-      const stream = fs.createReadStream(path.join(path_folder,file.name));
+    if (exten === '.css') {
+      const stream = fs.createReadStream(path.join(path_folder, file.name));
       stream.on('data', (str) => {
-        fs.appendFile(path.join(__dirname, 'project-dist/style.css'), `${str}${'\n'}`, () => {});
+        fs.appendFile(path.join(__dirname, 'project-dist/style.css'), `${str}${'\n'}`, () => { });
       });
     }
   });
 });
 
-// const stream = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
-// let string = '';
-// fs.createWriteStream(path.join(path_file_copy, 'index.html')).write('');
-// stream.on('data', (stream_read) => {
-//   string = stream_read.toString();
-//   fs.appendFile(path.join(path_file_copy, 'index.html'), `${string}${'\n'}`, () => {});
-// });
-// const path_comp = 
-// path.join(__dirname, 'components');
-// // const stream_art = fs.createReadStream(path.join(path_comp, 'articles.html'),  'utf-8');
-// // const stream_foo = fs.createReadStream(path.join(path_comp, 'footer.html'),  'utf-8');
-// const stream_head = fs.createReadStream(path.join(path_comp, 'header.html'));
-// let s = '';
-// const stream_ind = fs.createReadStream(path.join(path_file_copy, 'index.html'));
-// stream_ind.on('data', (str) => {
-//   console.log(str);
-//   s = str.toString();
-//   stream_head.on('data', (str) => {
-//     const str_head = str.toString();
-//     s.replace('{{header}}', str_head);
-//   });
-  
-  
-// });
+const stream = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+let string = '';
+
+const path_comp = path.join(__dirname, 'components');
+const stream_art = fs.createReadStream(path.join(path_comp, 'articles.html'));
+const stream_foo = fs.createReadStream(path.join(path_comp, 'footer.html'));
+fs.createWriteStream(path.join(path_file_copy, 'index.html')).write(string);
+const stream_head = fs.createReadStream(path.join(path_comp, 'header.html'));
+let str_head = '';
+let str_art = '';
+let str_foo = '';
+stream_head.on('data', (str) => {
+  str_head = str.toString();
+  stream_art.on('data', (str) => {
+    str_art = str.toString();
+    stream_foo.on('data', (str) => {
+      str_foo = str.toString();
+      stream.on('data', (stream_read) => {
+        string = stream_read.toString();
+        string = string.replace('{{header}}', str_head);
+        string = string.replace('{{articles}}', str_art);
+        string = string.replace('{{footer}}', str_foo);
+        
+        fs.appendFile(path.join(path_file_copy, 'index.html'), `${string}${'\n'}`, () => { });
+      });
+    });
+    
+  });
+});
